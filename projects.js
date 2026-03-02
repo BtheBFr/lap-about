3261175c1b7eed99cd054946768dfe8c302a6d2e
BtheBFr <rtfmqwertu29v@gmail.com>
2026-03-02 23:20:46 +0500

    Update projects.js

--- projects.js
+++ projects.js
@@ -10,14 +10,14 @@ const projectsData = [
     {
         title: "Lap Convert",
         titleSecondLine: "Конвертация",
-        description: "Образовательный хаб. Здесь будет много полезного для учебы.",
+        description: "Конвертируй что за хочешь.",
         link: "https://bthebfr.github.io/lap-convert/",
         status: "active",
         statusText: "Активно"  
     },
     {
-        title: "Work",
-        titleSecondLine: "Коллаборация с Заводом Осколки",
+        title: "Lap Files",
+        titleSecondLine: "Коллаборация с B the B и Заводом Осколки",
         description: "Совместный проект. Готовим что-то особенное.",
         link: "",
         status: "soon",
